17c826b10696dd3a10164a55dc550f5c0c77b92a
YNMDR <78961262+YNMDR@users.noreply.github.com>
2023-08-13 18:11:46 +0530

    Update image_color.py

--- image_color.py
+++ image_color.py
@@ -3,10 +3,13 @@ import cv2
 import numpy as np
 import color
 
-resized_image=np.zeros((720,512))
+resized_image=np.zeros((720,512,3))
 
 def display_color(res_img,c,x,y,b,g,r):
-    cv2.putText(res_img,"hjhj",(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(int(b),int(g),int(r)),5)
+    cv2.putText(res_img,c,(20,20),2,0.8,(255,255,255),1,cv2.LINE_AA)
+    if b+g+r>=600:
+        cv2.putText(res_img, c, (20, 20), 2, 0.8, (0,0,0), 1, cv2.LINE_AA)
+
 def call_back(event,x,y,flags,params):
     if event==cv2.EVENT_LBUTTONDOWN or event==cv2.EVENT_RBUTTONDOWN:
         blue_val=resized_image[y,x,0]
@@ -15,8 +18,8 @@ def call_back(event,x,y,flags,params):
         res_img = np.zeros((512, 512, 3), np.uint8)
         res_img[:]=[blue_val,green_val,red_val]
         color_rec=color.get_color(red_val, green_val, blue_val)
+        display_color(res_img, color_rec, 10, 50,int(blue_val),int(green_val),int(red_val))
         cv2.imshow("Chosen color",res_img)
-        display_color(res_img,color_rec, x, y, 0,0,0)
 
 def detect_color(image):
     global resized_image
