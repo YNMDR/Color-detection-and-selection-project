b3feb64a9d949ca6b19f43c5929df2bf2cb32c5e
YNMDR <78961262+YNMDR@users.noreply.github.com>
2023-08-13 18:14:35 +0530

    Update vid_color.py

--- vid_color.py
+++ vid_color.py
@@ -3,15 +3,22 @@ import numpy as np
 import cv2
 import color
 
-vid_frame=np.zeros((720,512))
+vid_frame=np.zeros((720,512,3))
+
+def display_color(res_img,c,x,y,b,g,r):
+    cv2.putText(res_img,c,(20,20),2,0.8,(255,255,255),1,cv2.LINE_AA)
+    if b+g+r>=600:
+        cv2.putText(res_img, c, (20, 20), 2, 0.8, (0,0,0), 1, cv2.LINE_AA)
+
 def call_back(event,x,y,flags,params):
     if event==cv2.EVENT_MOUSEMOVE:
         blue_val=vid_frame[y,x,0]
         green_val=vid_frame[y,x,1]
         red_val=vid_frame[y,x,2]
-        print(color.get_color(red_val,green_val,blue_val))
+        color_rec=(color.get_color(red_val,green_val,blue_val))
         res_img = np.zeros((512, 512, 3), np.uint8)
         res_img[:]=[blue_val,green_val,red_val]
+        display_color(res_img, color_rec, 10, 50, int(blue_val), int(green_val), int(red_val))
         cv2.imshow("Choosen color",res_img)
 
 def capture():
